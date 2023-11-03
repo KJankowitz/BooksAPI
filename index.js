3cab653ecc60a457a622df015605f91484fd54f6
Kardo Jankowitz <kardojv@gmail.com>
2023-11-03 13:07:28 +0200

    database connected, read functionality.

--- index.js
+++ index.js
@@ -6,11 +6,28 @@ import pg from "pg";
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "books",
+    password: "DizzyBean67",
+    port: 5432,
+  });
+  db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req,res) => {
-    res.render("index.ejs");
+let book_list = [];
+
+app.get("/", async(req,res) => {
+    const result = await db.query( 
+        "SELECT * FROM books ORDER BY TO_CHAR(date_completed :: DATE, 'dd/mm/yyyy') DESC"
+    );
+    book_list = result.rows;
+    res.render("index.ejs", {
+        list: book_list
+    });
 })
 
 app.listen(port, () => {
